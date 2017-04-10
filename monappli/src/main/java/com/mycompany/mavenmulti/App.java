ea922df011a20698b6992bee93987b36eace7f0a
vincent.salomon <vincent.salomon@zoho.com>
2017-04-10 17:27:37 +0200

    maven 10.04.2017

---
+++ monappli/src/main/java/com/mycompany/mavenmulti/App.java
@@ -0,0 +1,13 @@
+package com.mycompany.mavenmulti;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
